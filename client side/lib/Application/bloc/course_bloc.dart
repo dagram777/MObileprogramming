d03adb7f1d2467cb6aa2f8f4e6c4823a950539eb
fikirfikre <91533594+fikirfikre@users.noreply.github.com>
2022-06-05 18:42:06 +0300

    Update course_bloc.dart

--- client side/lib/Application/bloc/course_bloc.dart
+++ client side/lib/Application/bloc/course_bloc.dart
@@ -5,27 +5,25 @@ import 'package:flutter_application_9/Application/bloc/course_state.dart';
 import 'package:http/http.dart' as http;
 import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:flutter_application_9/Infrastructure/course/courseRepoistory.dart';
-// import 'package:stream_transform/stream_transform.dart';
+
 const _postLimit = 20;
 const throttleDuration = Duration(milliseconds: 100);
 
-// EventTransformer<E> throttleDroppable<E>(Duration duration) {
-//   return (events, mapper) {
-//     return droppable<E>().call(events.throttle(duration), mapper);
-//   };
-// }
 class CourseBloc extends Bloc<CourseEvent, CourseState> {
   CourseRepository courseRepository;
   CourseBloc(this.courseRepository) : super(CourseInitial()) {
   
-   on<CourseFetched>(_onCourseFetched);
-  //  transformer: throttleDroppable(throttleDuration),);
+    on<CourseFetched>(_onCourseFetched);
+    on<CourseCreate>(_oncourseCreate);
+    on<CourseUpdate>(_oncourseUpdate);
+    on<CourseDelete>(_oncourseDelete);
+  
   
   }
- // final http.Client httpClient;
+
 
 Future<void> _onCourseFetched(CourseFetched event,Emitter<CourseState> emit)async{
-  //if(state.hasReachedMax) return;
+
   try{
     
       final courses = await courseRepository.getCourses();
@@ -38,5 +36,39 @@ Future<void> _onCourseFetched(CourseFetched event,Emitter<CourseState> emit)asyn
   }
 
 }
+Future<void> _oncourseCreate(CourseCreate event,Emitter<CourseState> emit)async{
+    try {
+      final response = await courseRepository.createCourse(event.department,event.id,event.pdf,event.title,event.video,event.years);
+      emit(CourseCreated());
+      
+    } catch (e) {
+      emit(CourseFailed());
+    }
+
+  }
+
+  Future<void> _oncourseUpdate(CourseUpdate event,Emitter<CourseState> emit)async{
+    try {
+      final response = await courseRepository.updateCourse(event.department,event.id,event.pdf,event.title,event.video,event.years);
+      emit(CourseUpdated());
+      
+    } catch (e) {
+      emit(CourseFailed());
+    }
+
+  }
+
+  Future<void> _oncourseDelete(CourseDelete event,Emitter<CourseState> emit)async{
+    try {
+      final response = await courseRepository.deleteCourse(event.id);
+      emit(CourseDeleted());
+      
+    } catch (e) {
+      emit(CourseFailed());
+    }
+
+  }
+
 
 }
+
