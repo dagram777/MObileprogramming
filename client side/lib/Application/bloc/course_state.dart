565cc84c78fbd1cf83762ec2de7e68e174647907
fikirfikre <91533594+fikirfikre@users.noreply.github.com>
2022-06-05 18:43:57 +0300

    Update course_state.dart

--- client side/lib/Application/bloc/course_state.dart
+++ client side/lib/Application/bloc/course_state.dart
@@ -6,7 +6,7 @@ import 'package:flutter_application_9/Domain/Course.dart';
 
 abstract class CourseState extends Equatable{
   @override
-  // TODO: implement props
+  
   List<Object?> get props => throw UnimplementedError();
 
 }
@@ -29,37 +29,3 @@ class CourseUpdated extends CourseState{}
 
 class CourseDeleted extends CourseState{}
 
-// enum course_status {initial,success,failure}
-// class CourseState extends Equatable{
-//   final course_status status;
-//   final List<Course> courses;
-//   final bool hasReachedMax;
-//   const CourseState({
-//     this.status = course_status.initial,
-//     this.courses =const <Course>[],
-//     this.hasReachedMax = false,
-
-//   });
-  
-
-//   CourseState copyWith({
-//     course_status? status,
-//     List<Course>? courses,
-//     bool? hasReachedMax,
-
-
-//   }){
-//   return CourseState(
-//     status: status ?? this.status,
-//     courses: courses ?? this.courses,
-//     hasReachedMax: hasReachedMax ?? this.hasReachedMax
-//   );
-//   }
-//     String toString() {
-//     return '''CourseState { status: $status, hasReachedMax: $hasReachedMax, posts: ${courses.length} }''';
-//   }
-
-
-//   @override
-//   // TODO: implement props
-//   List<Object?> get props => [status,courses,hasReachedMax];} 
